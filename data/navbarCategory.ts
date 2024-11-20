6e105e964071625a8ff5119617c87a901a41b246
DESKTOP-P2V9UA1\SomeOne <molaei1999a@gmail.com>
2024-11-20 14:30:56 +0330

    feat: add navbar categories data

---
+++ data/navbarCategory.ts
@@ -0,0 +1,43 @@
+import { TbBeach } from "react-icons/tb";
+import { GiWindmill } from "react-icons/gi";
+import { MdOutlineVilla } from "react-icons/md";
+import { TbMountain } from "react-icons/tb";
+import { TbPool } from "react-icons/tb";
+import { GiIsland } from "react-icons/gi";
+import { GiBoatFishing } from "react-icons/gi";
+import { FaSkiing } from "react-icons/fa";
+import { GiCastle } from "react-icons/gi";
+import { GiForestCamp } from "react-icons/gi";
+import { BsSnow } from "react-icons/bs";
+import { GiCaveEntrance } from "react-icons/gi";
+import { GiCactus } from "react-icons/gi";
+import { GiBarn } from "react-icons/gi";
+import { TbSailboat } from "react-icons/tb";
+import { IoDiamond } from "react-icons/io5";
+import { PiParkFill } from "react-icons/pi";
+import { TbPlayVolleyball } from "react-icons/tb";
+import { GiModernCity } from "react-icons/gi";
+import { GiGrandPiano } from "react-icons/gi";
+
+export const categories = [
+  { id: 1, label: "Beach", icons: TbBeach },
+  { id: 2, label: "Windmill", icons: GiWindmill },
+  { id: 3, label: "Modern", icons: MdOutlineVilla },
+  { id: 4, label: "Countryside", icons: TbMountain },
+  { id: 5, label: "Pools", icons: TbPool },
+  { id: 6, label: "Islands", icons: GiIsland },
+  { id: 7, label: "Lake", icons: GiBoatFishing },
+  { id: 8, label: "Skiing", icons: FaSkiing },
+  { id: 9, label: "Castles", icons: GiCastle },
+  { id: 10, label: "camping", icons: GiForestCamp },
+  { id: 11, label: "arctic", icons: BsSnow },
+  { id: 12, label: "Cave", icons: GiCaveEntrance },
+  { id: 13, label: "Desert", icons: GiCactus },
+  { id: 14, label: "Barns", icons: GiBarn },
+  { id: 15, label: "Lux", icons: IoDiamond },
+  { id: 16, label: "Boats", icons: TbSailboat },
+  { id: 17, label: "National parks", icons: PiParkFill },
+  { id: 18, label: "Plays", icons: TbPlayVolleyball },
+  { id: 19, label: "Top Cities", icons: GiModernCity },
+  { id: 20, label: "Grand Pianos", icons: GiGrandPiano },
+];
